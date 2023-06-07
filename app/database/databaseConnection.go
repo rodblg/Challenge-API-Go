9d404541bfd7dd22f3b2a4f4ce3b12df4ecf0f9b
rodblg <rodrigoblancas19@gmail.com>
2023-06-07 12:17:13 -0600

    change mongodb uri in database connection

--- app/database/databaseConnection.go
+++ app/database/databaseConnection.go
@@ -6,19 +6,26 @@ import (
 	"log"
 	"os"
 	"time"
+	"github.com/joho/godotenv"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DBinstance() *mongo.Client {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Some error occured while reading .env file")
+	}
 	
 	MongoDB := os.Getenv("MONGO_URI")
 	if MongoDB == "" {
-		MongoDB = "mongodb://admin:password@mongodb"
+		MONGO_INITDB_ROOT_USERNAME := os.Getenv("MONGO_INITDB_ROOT_USERNAME")
+		MONGO_INITDB_ROOT_PASSWORD := os.Getenv("MONGO_INITDB_ROOT_PASSWORD")
+		MongoDB = fmt.Sprintf("mongodb://%s:%s@mongodb", MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD)
 	}
 
-	fmt.Print(MongoDB)
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
 	if err != nil {
